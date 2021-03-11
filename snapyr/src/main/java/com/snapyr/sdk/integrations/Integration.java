038ec09ebf9ed9bae6271370cae3495525a01d10
agrigoryan <aram.grigoryan@gmail.com>
2021-03-11 16:44:15 -0500

    rename packaging, remove wear sdk

---
+++ snapyr/src/main/java/com/snapyr/sdk/integrations/Integration.java
@@ -0,0 +1,107 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Segment.io, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.snapyr.sdk.integrations;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.snapyr.sdk.Options;
+import com.snapyr.sdk.Properties;
+import com.snapyr.sdk.Snapyr;
+import com.snapyr.sdk.Traits;
+import com.snapyr.sdk.ValueMap;
+
+/**
+ * Converts Snapyr messages to a format a bundled integration understands, and calls those methods.
+ *
+ * @param <T> The type of the backing instance. This isn't strictly necessary (since we return an
+ *     object), but serves as documentation for what type to expect with {@link
+ *     #getUnderlyingInstance()}.
+ */
+public abstract class Integration<T> {
+
+    public interface Factory {
+
+        /**
+         * Attempts to create an adapter for with {@code settings}. This returns the adapter if one
+         * was created, or null if this factory isn't capable of creating such an adapter.
+         */
+        Integration<?> create(ValueMap settings, Snapyr analytics);
+
+        /** The key for which this factory can create an {@link Integration}. */
+        @NonNull
+        String key();
+    }
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivityStarted(Activity activity) {}
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivityResumed(Activity activity) {}
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivityPaused(Activity activity) {}
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivityStopped(Activity activity) {}
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+    /** @see android.app.Application.ActivityLifecycleCallbacks */
+    public void onActivityDestroyed(Activity activity) {}
+
+    /** @see Snapyr#identify(String, Traits, Options) */
+    public void identify(IdentifyPayload identify) {}
+
+    /** @see Snapyr#group(String, Traits, Options) */
+    public void group(GroupPayload group) {}
+
+    /** @see Snapyr#track(String, Properties, Options) */
+    public void track(TrackPayload track) {}
+
+    /** @see Snapyr#alias(String, Options) */
+    public void alias(AliasPayload alias) {}
+
+    /** @see Snapyr#screen(String, String, Properties, Options) */
+    public void screen(ScreenPayload screen) {}
+
+    /** @see Snapyr#flush() */
+    public void flush() {}
+
+    /** @see Snapyr#reset() */
+    public void reset() {}
+
+    /**
+     * The underlying instance for this provider - used for integration specific actions. This will
+     * return {@code null} for SDK's that only provide interactions with static methods (e.g.
+     * Localytics).
+     */
+    public T getUnderlyingInstance() {
+        return null;
+    }
+}
---
+++ snapyr/src/main/java/com/snapyr/sdk/integrations/TrackPayload.java
@@ -0,0 +1,161 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Segment.io, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.snapyr.sdk.integrations;
+
+import static com.snapyr.sdk.internal.Utils.assertNotNull;
+import static com.snapyr.sdk.internal.Utils.assertNotNullOrEmpty;
+import static com.snapyr.sdk.internal.Utils.isNullOrEmpty;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.snapyr.sdk.Properties;
+import com.snapyr.sdk.internal.Private;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TrackPayload extends BasePayload {
+
+    static final String EVENT_KEY = "event";
+    static final String PROPERTIES_KEY = "properties";
+
+    @Private
+    TrackPayload(
+            @NonNull String messageId,
+            @NonNull Date timestamp,
+            @NonNull Map<String, Object> context,
+            @NonNull Map<String, Object> integrations,
+            @Nullable String userId,
+            @NonNull String anonymousId,
+            @NonNull String event,
+            @NonNull Map<String, Object> properties,
+            boolean nanosecondTimestamps) {
+        super(
+                Type.track,
+                messageId,
+                timestamp,
+                context,
+                integrations,
+                userId,
+                anonymousId,
+                nanosecondTimestamps);
+        put(EVENT_KEY, event);
+        put(PROPERTIES_KEY, properties);
+    }
+
+    /**
+     * The name of the event. We recommend using title case and past tense for event names, like
+     * "Signed Up".
+     */
+    @NonNull
+    public String event() {
+        return getString(EVENT_KEY);
+    }
+
+    /**
+     * A dictionary of properties that give more information about the event. We have a collection
+     * of special properties that we recognize with semantic meaning. You can also add your own
+     * custom properties.
+     */
+    @NonNull
+    public Properties properties() {
+        return getValueMap(PROPERTIES_KEY, Properties.class);
+    }
+
+    @Override
+    public String toString() {
+        return "TrackPayload{event=\"" + event() + "\"}";
+    }
+
+    @NonNull
+    @Override
+    public TrackPayload.Builder toBuilder() {
+        return new Builder(this);
+    }
+
+    /** Fluent API for creating {@link TrackPayload} instances. */
+    public static class Builder extends BasePayload.Builder<TrackPayload, Builder> {
+
+        private String event;
+        private Map<String, Object> properties;
+
+        public Builder() {
+            // Empty constructor.
+        }
+
+        @Private
+        Builder(TrackPayload track) {
+            super(track);
+            event = track.event();
+            properties = track.properties();
+        }
+
+        @NonNull
+        public Builder event(@NonNull String event) {
+            this.event = assertNotNullOrEmpty(event, "event");
+            return this;
+        }
+
+        @NonNull
+        public Builder properties(@NonNull Map<String, ?> properties) {
+            assertNotNull(properties, "properties");
+            this.properties = Collections.unmodifiableMap(new LinkedHashMap<>(properties));
+            return this;
+        }
+
+        @Override
+        protected TrackPayload realBuild(
+                @NonNull String messageId,
+                @NonNull Date timestamp,
+                @NonNull Map<String, Object> context,
+                @NonNull Map<String, Object> integrations,
+                String userId,
+                @NonNull String anonymousId,
+                boolean nanosecondTimestamps) {
+            assertNotNullOrEmpty(event, "event");
+
+            Map<String, Object> properties = this.properties;
+            if (isNullOrEmpty(properties)) {
+                properties = Collections.emptyMap();
+            }
+
+            return new TrackPayload(
+                    messageId,
+                    timestamp,
+                    context,
+                    integrations,
+                    userId,
+                    anonymousId,
+                    event,
+                    properties,
+                    nanosecondTimestamps);
+        }
+
+        @Override
+        Builder self() {
+            return this;
+        }
+    }
+}
