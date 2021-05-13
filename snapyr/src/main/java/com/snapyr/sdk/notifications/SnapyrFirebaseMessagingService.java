c1a0308a3cc7870375860588477ca24051cd2996
paulwsmith <paul.smith@games24x7.com>
2021-05-13 15:52:24 -0400

    WIP: Base Firebase receiver

---
+++ snapyr/src/main/java/com/snapyr/sdk/notifications/SnapyrFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.snapyr.sdk.notifications;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class SnapyrFirebaseMessagingService extends FirebaseMessagingService {
+
+}
