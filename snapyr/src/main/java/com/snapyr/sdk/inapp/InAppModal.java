26875e8c3e42e089d0eb717448bbd9add2c93ff8
Brandon Smith <smith.brandon.63@gmail.com>
2022-08-04 12:41:39 -0400

    cleanup

--- snapyr/src/main/java/com/snapyr/sdk/inapp/InAppModal.java
+++ snapyr/src/main/java/com/snapyr/sdk/inapp/InAppModal.java
@@ -9,26 +9,27 @@ import android.util.DisplayMetrics;
 import android.webkit.WebView;
 
 public class InAppModal {
-    static boolean showingModal = false;
+    static boolean showingModal = false; // super lazy modal logic
 
     public static void ShowPopup(Activity activity, String rawHtml) {
         if (InAppModal.showingModal == true){
             return;
         }
-       InAppModal.showingModal = true;
-       // DisplayMetrics displayMetrics = new DisplayMetrics();
+        InAppModal.showingModal = true;
+        WebView v = new WebView(activity);
+
+        // doing this seems to make the the webpage super jittery, so ignore unless we need later
+        // DisplayMetrics displayMetrics = new DisplayMetrics();
         //activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         //int height = displayMetrics.heightPixels;
         //int width = displayMetrics.widthPixels;
+        // v.layout(0, 0, height, width);
 
-        WebView v = new WebView(activity);
         //v.canGoBackOrForward(0);
-       // v.layout(0, 0, height, width);
 
-
+        // I don't know why this needs to be base64 but w/e
         String encodedHtml = Base64.encodeToString(rawHtml.getBytes(), Base64.NO_PADDING);
         v.loadData(encodedHtml, "text/html", "base64");
-        //v.setOn
 
         AlertDialog dialog = new AlertDialog.Builder(activity).setView(v).create();
         dialog.setMessage("Brandon is cool");
