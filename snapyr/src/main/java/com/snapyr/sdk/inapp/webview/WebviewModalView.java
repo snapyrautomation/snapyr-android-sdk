41ef22cee7c85d1d783a3f21a9e68c0470907079
Brandon Smith <smith.brandon.63@gmail.com>
2022-09-09 11:53:59 -0400

    Update WebviewModalView.java

--- snapyr/src/main/java/com/snapyr/sdk/inapp/webview/WebviewModalView.java
+++ snapyr/src/main/java/com/snapyr/sdk/inapp/webview/WebviewModalView.java
@@ -24,26 +24,18 @@
 package com.snapyr.sdk.inapp.webview;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
-import android.util.AttributeSet;
 import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-
-import androidx.fragment.app.DialogFragment;
-
 import com.snapyr.sdk.ValueMap;
 import com.snapyr.sdk.core.R;
 import com.snapyr.sdk.services.ServiceFacade;
@@ -57,8 +49,7 @@ public class WebviewModalView extends FrameLayout {
             return;
         }
 
-        activity.runOnUiThread(() ->
-                new WebviewModalView(activity, rawHtml));
+        activity.runOnUiThread(() -> new WebviewModalView(activity, rawHtml));
     }
 
     public WebviewModalView(Context context, String html) {
@@ -69,9 +60,10 @@ public class WebviewModalView extends FrameLayout {
         WebView view = configureWebview(context);
 
         ImageButton dismissButton = this.findViewById(R.id.dismiss_button);
-        dismissButton.setOnClickListener(view1 -> {
-            activeDialog.dismiss();
-        });
+        dismissButton.setOnClickListener(
+                view1 -> {
+                    activeDialog.dismiss();
+                });
 
         // I don't know why this needs to be base64 but w/e
         String encodedHtml = Base64.encodeToString(html.getBytes(), Base64.NO_PADDING);
@@ -81,13 +73,11 @@ public class WebviewModalView extends FrameLayout {
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
 
         activeDialog = new PopupWindow(this, width, height, true);
-        activeDialog.setOnDismissListener(
-                () -> WebviewModal.showingModal = false);
-        activeDialog.setBackgroundDrawable(new ColorDrawable(
-                android.graphics.Color.TRANSPARENT));
+        activeDialog.setOnDismissListener(() -> WebviewModal.showingModal = false);
+        activeDialog.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
     }
 
-    private InAppWebviewClient createClient(){
+    private InAppWebviewClient createClient() {
         return new InAppWebviewClient() {
             @Override
             public void onPageCommitVisible(WebView view, String url) {
@@ -104,19 +94,22 @@ public class WebviewModalView extends FrameLayout {
                 // Set dialog width to MATCH_PARENT, in this case meaning
                 // maximum possible
                 // width for the display
-                //WindowManager.LayoutParams attributes = new WindowManager.LayoutParams();
-                //attributes.copyFrom(activeDialog.getWindow().getAttributes());
-                //attributes.width = WindowManager.LayoutParams.MATCH_PARENT;
-                //activeDialog.getWindow().setAttributes(attributes);
+                // WindowManager.LayoutParams attributes = new WindowManager.LayoutParams();
+                // attributes.copyFrom(activeDialog.getWindow().getAttributes());
+                // attributes.width = WindowManager.LayoutParams.MATCH_PARENT;
+                // activeDialog.getWindow().setAttributes(attributes);
             }
         };
     }
-    private WebView configureWebview(Context context){
+
+    private WebView configureWebview(Context context) {
         WebView wv = this.findViewById(R.id.webview);
         DisplayMetrics displayMetrics = new DisplayMetrics();
         int defaultMargin = 44; // we should get this from somewhere real but it's the PERFECT
         ServiceFacade.getCurrentActivity()
-                .getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                .getWindowManager()
+                .getDefaultDisplay()
+                .getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels - (defaultMargin * 2);
 
         wv.setWebViewClient(createClient());
